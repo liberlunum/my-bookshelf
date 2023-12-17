2f1751ab8a71b97eeda1e88ccfd9581a96fb0767
liberlunum <dghezeriiti@gmail.com>
2023-12-17 20:17:10 +0300

    deleted some book card

--- src/components/body/library/Library.js
+++ src/components/body/library/Library.js
@@ -228,62 +228,8 @@ function Library() {
                 </div>
 
             </div>
-            <div className={styles.oneBookCard}>
-                <img src={book} alt="book" className={styles.coverBook}/>
-                <div>
-                    <div>
-                        <input type="checkbox" id="read" name="read"/>
-                        <label htmlFor="read">Прочитано</label>
-                    </div>
-                    <div>
-                        <input type="checkbox" id="not-read" name="not-read"/>
-                        <label htmlFor="not-read">Не прочитано</label>
-                    </div>
-                </div>
+        
 
-            </div>
-            <div className={styles.oneBookCard}>
-                <img src={book} alt="book" className={styles.coverBook}/>
-                <div>
-                    <div>
-                        <input type="checkbox" id="read" name="read"/>
-                        <label htmlFor="read">Прочитано</label>
-                    </div>
-                    <div>
-                        <input type="checkbox" id="not-read" name="not-read"/>
-                        <label htmlFor="not-read">Не прочитано</label>
-                    </div>
-                </div>
-
-            </div>
-            <div className={styles.oneBookCard}>
-                <img src={book} alt="book" className={styles.coverBook}/>
-                <div>
-                    <div>
-                        <input type="checkbox" id="read" name="read"/>
-                        <label htmlFor="read">Прочитано</label>
-                    </div>
-                    <div>
-                        <input type="checkbox" id="not-read" name="not-read"/>
-                        <label htmlFor="not-read">Не прочитано</label>
-                    </div>
-                </div>
-
-            </div>
-            <div className={styles.oneBookCard}>
-                <img src={book} alt="book" className={styles.coverBook}/>
-                <div>
-                    <div>
-                        <input type="checkbox" id="read" name="read"/>
-                        <label htmlFor="read">Прочитано</label>
-                    </div>
-                    <div>
-                        <input type="checkbox" id="not-read" name="not-read"/>
-                        <label htmlFor="not-read">Не прочитано</label>
-                    </div>
-                </div>
-
-            </div>
 
 
 
